32eaa2f3b04dfbec5fee3e1fe0741637e757d583
Williams392 <williamsvalle392@gmail.com>
2023-06-03 12:13:08 -0500

    19

--- core/products/views.py
+++ core/products/views.py
@@ -4,7 +4,6 @@ from rest_framework.parsers import MultiPartParser, FormParser  # img
 from django.shortcuts import get_object_or_404  # hoy -> id
 
 from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
