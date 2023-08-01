b7f0ee1302961fc0aaafea46cf6d9076a6d97631
Williams392 <williamsvalle392@gmail.com>
2023-07-31 19:56:20 -0500

    mejorando el admin para subir fotos

--- core/products/admin.py
+++ core/products/admin.py
@@ -1,15 +1,39 @@
 # 4
 
 from django.contrib import admin
-from .models import Product
+from .models import Product, Photo, Tag, Category
 
 
+class PhotoInline(admin.StackedInline):
+    model = Photo
+
+
+class TagInline(admin.TabularInline):
+    model = Tag.product_set.through
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.product_set.through
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline, TagInline, CategoryInline]
+
+
+admin.site.register(Photo)
+admin.site.register(Tag)
+admin.site.register(Category)
+
+
+'''
+    YO ise esto:
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'product', 'brand', 'price', 'description', 'image')
     ordering = ('-id',)
 
 
 admin.site.register(Product, ProductAdmin)
+'''
 
 
 # Para visualizal todos los dantos en el Django administration
--- core/products/models.py
+++ core/products/models.py
@@ -1,10 +1,51 @@
 # (3) models.py
 
 from django.db import models
+from django.conf import settings
 
 
 class Product(models.Model):
 
+    STATUS_CHOICES = {
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    }
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = models.ManyToManyField('Tag')
+    categories = models.ManyToManyField('Category')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Photo(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='photos')
+    image = models.ImageField(upload_to='product_photos')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    '''
+    YO ise esto:
     product = models.CharField(max_length=255, unique=True)
 
     brand = models.CharField(
@@ -22,6 +63,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product
+    '''
 
 
 '''
